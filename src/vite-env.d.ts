d7b515d1bfa6fbe41c60ecfbbe1f6fc45f220938
Aminerc <aminerc.business@outlook.com>
2025-04-09 18:27:55 +0200

    Update emailing system

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
     readonly VITE_EMAILJS_SERVICE_ID: string;
     readonly VITE_EMAILJS_TEMPLATE_ID: string;
-    readonly VITE_EMAILJS_ACCESS_TOKEN: string;
+    readonly VITE_EMAILJS_PUBLIC_KEY: string;
   }
   
   interface ImportMeta {
